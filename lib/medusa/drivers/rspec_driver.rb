1a653724264a0dd9a70b5edb3e5f1cec4b7d1f86
Sean St. Quentin <sean.st.quentin@envato.com>
2014-03-24 11:47:05 +1100

    Don't wipe the loaded spec_helper configuration when running multiple spec files sequentially.

--- lib/medusa/drivers/rspec_driver.rb
+++ lib/medusa/drivers/rspec_driver.rb
@@ -18,17 +18,51 @@ module Medusa
 
         medusa_output.on_output do |message|
           message_bus.write Messages::Runner::Results.new(output: message, file: file)
+        end        
+
+        setup_environment(file, medusa_output)
+
+        @configuration.reporter.report(@world.example_count, @configuration.randomize? ? @configuration.seed : nil) do |reporter|
+          begin
+            @configuration.run_hook(:before, :suite)
+            @world.example_groups.ordered.map {|g| g.run(reporter)}.all? ? 0 : @configuration.failure_exit_code
+          ensure
+            @configuration.run_hook(:after, :suite)
+          end
+        end
+      end
+
+      private
+
+      def parse_options(options)
+        if Array === options
+          options = RSpec::Core::ConfigurationOptions.new(options)
+          options.parse_options
         end
 
-        config = [
-          '-f',
-          'RSpec::Core::Formatters::MedusaFormatter',
-          file
-        ]
+        options
+      end
 
-        RSpec.instance_variable_set(:@world, nil)
+      def setup_environment(file, output)
+        unless @core_config
+          @core_config   = RSpec::configuration.dup
+        end
 
-        RSpec::Core::Runner.run(config, medusa_output, medusa_output)
+        @configuration = @core_config.dup
+
+        @configuration.error_stream = output
+        @configuration.output_stream = output
+
+        @configuration.instance_variable_set("@formatters", nil)
+        @configuration.instance_variable_set("@reporter", nil)
+        @configuration.formatter = 'RSpec::Core::Formatters::MedusaFormatter'
+
+        @configuration.files_to_run = [file.to_s]
+
+        @world = RSpec.world = RSpec::Core::World.new(@configuration)
+
+        @configuration.load_spec_files
+        @world.announce_filters
       end
     end
   end
