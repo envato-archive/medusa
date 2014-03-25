3c5be9b19f63dd14721b5a62df670093f9e41266
Pablo Lee <pablo.lee@envato.com>
2014-03-25 21:42:18 +1100

    Only create the teamcity class if it teamcity libs are available

--- lib/medusa/teamcity/messenger.rb
+++ lib/medusa/teamcity/messenger.rb
@@ -4,63 +4,65 @@ begin
 rescue LoadError
 end
 
-module Medusa
-  module Teamcity
-    class Messenger
-      include ::Rake::TeamCity::RunnerCommon
+if defined?(::Rake::TeamCity::RunnerCommon)
+  module Medusa
+    module Teamcity
+      class Messenger
+        include ::Rake::TeamCity::RunnerCommon
 
-      def notify_example_group_started(example_group)
-        count = example_group.count
-        description = example_group.description
+        def notify_example_group_started(example_group)
+          count = example_group.count
+          description = example_group.description
 
-        # send_msg(::Rake::TeamCity::MessageFactory.create_progress_message("Starting.. (#{count} examples)"))
-        # send_msg(::Rake::TeamCity::MessageFactory.create_suite_started(description, ''))
-      end
+          # send_msg(::Rake::TeamCity::MessageFactory.create_progress_message("Starting.. (#{count} examples)"))
+          # send_msg(::Rake::TeamCity::MessageFactory.create_suite_started(description, ''))
+        end
 
-      def notify_example_group_finished(example_group)
-        description = example_group.description
-        # send_msg(::Rake::TeamCity::MessageFactory.create_suite_finished(description))
-        # send_msg(totals)
-      end
+        def notify_example_group_finished(example_group)
+          description = example_group.description
+          # send_msg(::Rake::TeamCity::MessageFactory.create_suite_finished(description))
+          # send_msg(totals)
+        end
 
-      def notify_example_started(example)
-        name = example.name
-        # send_msg(::Rake::TeamCity::MessageFactory.create_test_started(name, ''))
-      end
+        def notify_example_started(example)
+          name = example.name
+          # send_msg(::Rake::TeamCity::MessageFactory.create_test_started(name, ''))
+        end
 
-      def notify_example_finished(example)
-        name = example.name
-        duration = example.duration
+        def notify_example_finished(example)
+          name = example.name
+          duration = example.duration
 
-        # if stdout_string && !stdout_string.empty?
-        #   send_msg(::Rake::TeamCity::MessageFactory.create_test_output_message(name, true, stdout_string))
-        # end
-        #
-        # if stderr_string && !stderr_string.empty?
-        #   send_msg(::Rake::TeamCity::MessageFactory.create_test_output_message(name, false, stderr_string))
-        # end
+          # if stdout_string && !stdout_string.empty?
+          #   send_msg(::Rake::TeamCity::MessageFactory.create_test_output_message(name, true, stdout_string))
+          # end
+          #
+          # if stderr_string && !stderr_string.empty?
+          #   send_msg(::Rake::TeamCity::MessageFactory.create_test_output_message(name, false, stderr_string))
+          # end
 
-        # notify_success or failure or pending
+          # notify_success or failure or pending
 
-        # send_msg(::Rake::TeamCity::MessageFactory.create_test_finished(name, duration, nil))
-      end
+          # send_msg(::Rake::TeamCity::MessageFactory.create_test_finished(name, duration, nil))
+        end
 
-      private
+        private
 
-      def notify_success(result)
+        def notify_success(result)
 
-      end
+        end
 
-      def notify_failure(result)
-        name = result.description
-        exception = result.exception
-        backtrace = result.exception_backtrace
-        # send_msg(::Rake::TeamCity::MessageFactory.create_test_failed(name, exception, backtrace))
-      end
+        def notify_failure(result)
+          name = result.description
+          exception = result.exception
+          backtrace = result.exception_backtrace
+          # send_msg(::Rake::TeamCity::MessageFactory.create_test_failed(name, exception, backtrace))
+        end
 
-      def notify_pending(result)
-        name = result.description
-        # send_msg(::Rake::TeamCity::MessageFactory.create_test_ignored(name, ''))
+        def notify_pending(result)
+          name = result.description
+          # send_msg(::Rake::TeamCity::MessageFactory.create_test_ignored(name, ''))
+        end
       end
     end
   end
