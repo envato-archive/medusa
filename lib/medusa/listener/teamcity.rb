d83c7c9e173a0c3853895d8a541d91b4ea848cb9
Pablo Lee <pablo.lee@envato.com>
2014-03-26 09:45:12 +1100

    Only initialise TeamCity stuff if teamcity libs present

--- lib/medusa/listener/teamcity.rb
+++ lib/medusa/listener/teamcity.rb
@@ -1,29 +1,31 @@
-module Medusa
-  module Listener
-    class Teamcity < Medusa::Listener::Abstract
-      def initialize(output = $stdout)
-        super(output)
-        @teamcity_messenger = Medusa::Teamcity::Messenger.new
-      end
+if defined?(Medusa::Teamcity::Messenger)
+  module Medusa
+    module Listener
+      class Teamcity < Medusa::Listener::Abstract
+        def initialize(output = $stdout)
+          super(output)
+          @teamcity_messenger = Medusa::Teamcity::Messenger.new
+        end
 
-      def example_group_started(group_name)
-        @teamcity_messenger.notify_example_group_started(group_name)
-      end
+        def example_group_started(group_name)
+          @teamcity_messenger.notify_example_group_started(group_name)
+        end
 
-      def example_group_finished(group_name)
-        @teamcity_messenger.notify_example_group_finished(group_name)
-      end
+        def example_group_finished(group_name)
+          @teamcity_messenger.notify_example_group_finished(group_name)
+        end
 
-      def file_summary(summary)
-        @teamcity_messenger.notify_example_group_summary(summary)
-      end
+        def file_summary(summary)
+          @teamcity_messenger.notify_example_group_summary(summary)
+        end
 
-      def example_begin(example_name)
-        @teamcity_messenger.notify_example_started(example_name)
-      end
+        def example_begin(example_name)
+          @teamcity_messenger.notify_example_started(example_name)
+        end
 
-      def result_received(file, result)
-        @teamcity_messenger.notify_example_finished(file, result)
+        def result_received(file, result)
+          @teamcity_messenger.notify_example_finished(file, result)
+        end
       end
     end
   end
